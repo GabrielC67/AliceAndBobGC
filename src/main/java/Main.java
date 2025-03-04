b2b6cefeb6bc281a264285f0de00156ec222c5bb
GabrielC67 <gabecruzpro@gmail.com>
2025-03-04 18:43:54 -0500

    Testing phase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,34 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
 public class Main {
+    public static void main(String[] args){
 
-    public static void main(String[] args ){
         // create a new object of class Main
-
+        Main myObj = new Main();
         // call the compute() method on that new object
+        myObj.compute();
     }
 
     public void compute() {
         // Get input string of name from user
-        
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
         // Create conditional to check name against Alice and Bob
+        final String alice = "Alice";
+        final String bob = "Bob";
+        if (input.equals(alice)){
+            System.out.println("Hello, Alice!");
+        } else if (input.equals(bob)){
+            System.out.println("Hello, Bob!");
+        } else {
+            System.out.println("You are neither Alice nor Bob.");
+        }
+
+        return;
 
         // Print greeting to screen if Alice or Bob are true
         
